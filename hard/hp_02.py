9febe195be6b40eeada6fe3438be95f860b72ad8
Sarus1997 <saharat.rus@gmail.com>
2025-03-29 17:40:31 +0700

    update

--- hard/hp_02.py
+++ hard/hp_02.py
@@ -1,5 +1,8 @@
 
 # โจทย์: แก้ไขโค้ดต่อไปนี้ให้ถูกต้อง
+# โดยให้โค้ดทำงานได้ทั้งใน Windows และ Linux
+# และให้ทำงานได้ทั้งในรูปแบบของ Thread และ Process
+# โดยไม่ให้เกิดข้อผิดพลาดใดๆ
 
 """
     import multiprocessing
@@ -22,3 +25,20 @@
 """
 
 # คำตอบ:
+import multiprocessing
+import threading
+
+def worker():
+    print("Working")
+
+if __name__ == "__main__":
+    for _ in range(5):
+        t = threading.Thread(target=worker)
+        p = multiprocessing.Process(target=worker)
+        
+        t.start()
+        p.start()
+        
+        t.join()
+        p.join()
+
